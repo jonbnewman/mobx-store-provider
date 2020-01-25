974c18d35bb53371f591ea41c2eef01e2b528a99
Jonathan Newman <jbnewm@gmail.com>
2020-01-25 14:39:47 -0600

    added dockblock explaining createStore, updated copy

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,11 @@ export interface IStoreProvider {
   useStore: Function;
 }
 
+/**
+ * React Hook used to instantiate a mobx-state-tree store from within a component.
+ * @param storeFactory Callback used to create your mobx-state-tree store
+ * @returns The instance created by your `storeFactory` function
+ */
 export function createStore(storeFactory: Function): any {
   const storeRef = useRef(null);
   if (!storeRef.current) {
@@ -21,6 +26,7 @@ export function createStore(storeFactory: Function): any {
 /**
  * Create a new StoreProvider instance, which supplies you with a `Provider`, `Consumer`, and a `useStore` hook.
  * @param defaultValue The default value you want supplied to consumers of useStore in the event no Provider is found (null by default)
+ * @returns An IStoreProvider object which contains the Provider, Consumer, and a useStore hook
  */
 export default function StoreProvider(defaultValue = null): IStoreProvider {
   const StoreContext = React.createContext(defaultValue);
