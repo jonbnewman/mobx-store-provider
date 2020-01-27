932d0b8545eb84a3defce8de286df3fcf5bec7b3
Jonathan Newman <jbnewm@gmail.com>
2020-01-27 06:36:48 -0600

    misc reorg

--- src/stores.ts
+++ src/stores.ts
@@ -8,6 +8,10 @@ interface Store {
 
 const stores = new Map();
 
+function identity(thing: any): any {
+  return thing;
+}
+
 /**
  * Creates and/or retrieves the `store` from the internal `stores` Map.
  * @param storeIdentifier The identifier supplied by the consumer
@@ -25,10 +29,6 @@ function retrieveStore(storeIdentifier: any = null): Store {
     });
   }
   return stores.get(storeIdentifier);
-}
-
-function identity(thing: any): any {
-  return thing;
 }
 
 export { retrieveStore };
