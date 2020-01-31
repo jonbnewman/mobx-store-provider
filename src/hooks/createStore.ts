23f3418bcb99eae34e674bd7ad1d99c2dab52ccf
Jonathan Newman <jbnewm@gmail.com>
2020-01-30 19:16:49 -0600

    fixed factory function reference in docblock

--- src/hooks/createStore.ts
+++ src/hooks/createStore.ts
@@ -6,7 +6,7 @@ import { StoreFactory } from "../types";
  * React Hook used to instantiate a new store from within a component.
  * @param factory Callback used to create and return a store
  * @param storeIdentifier The identifier used for the store (optional)
- * @returns The instance created by the `storeFactory` function
+ * @returns The instance created by the `factory` function
  */
 function createStore(factory: StoreFactory, storeIdentifier: any = null): any {
   useEffect(() => () => retrieveStore(storeIdentifier).disposeStore(), []);
