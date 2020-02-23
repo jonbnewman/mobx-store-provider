97d241ee657b08958f1ef750af78b7f18a323ffe
Jonathan Newman <jbnewm@gmail.com>
2020-02-23 17:05:24 -0600

    removed unnecessary setName action

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -2,15 +2,9 @@ import { types, Instance } from "mobx-state-tree";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-const TestStore = types
-  .model({
-    name: types.optional(types.string, "TestStore"),
-  })
-  .actions(self => ({
-    setName(name: string) {
-      self.name = name;
-    },
-  }));
+const TestStore = types.model({
+  name: types.optional(types.string, "TestStore"),
+});
 
 interface ITestStore extends Instance<typeof TestStore> {}
 
